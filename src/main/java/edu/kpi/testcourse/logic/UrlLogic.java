0dd5cd335572fec4bee66ea5829d1d1c4e5c4ab8
Andrii Protsenko <a.protsenko@pnn.in.ua>
2021-03-11 12:59:52 +0200

    add UrlLogic structure

---
+++ src/main/java/edu/kpi/testcourse/logic/UrlLogic.java
@@ -0,0 +1,16 @@
+package edu.kpi.testcourse.logic;
+
+public class UrlLogic {
+  UrlLogic(){
+
+  }
+  public void getAllUrls(){
+
+  }
+  public void deleteUrlAlias(){
+
+  }
+  public void shortUrl(){
+
+  }
+}
