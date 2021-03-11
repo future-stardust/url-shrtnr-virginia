6f4e51c7f85f6b228ee2bb746b809a1b373b5c30
Andrii Protsenko <a.protsenko@pnn.in.ua>
2021-03-11 11:29:14 +0200

    add addUser functionality

--- src/main/java/edu/kpi/testcourse/store/DataStore.java
+++ src/main/java/edu/kpi/testcourse/store/DataStore.java
@@ -1,18 +1,39 @@
 package edu.kpi.testcourse.store;
 
+import com.nimbusds.jose.shaded.json.JSONArray;
+import com.nimbusds.jose.shaded.json.JSONObject;
 import edu.kpi.testcourse.auth.Url;
 import edu.kpi.testcourse.auth.User;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
-class DataStore  {
+public class DataStore  {
 
-  private String fileName;
+  private final String fileName = "store.txt";
+  private FileWriter fileWriter;
+  JSONObject obj = new JSONObject();
+  JSONArray users_list = new JSONArray();
+  JSONArray links_list = new JSONArray();
 
   public DataStore() {
+    User user1 = new User("test1@mail.com", "hjhfdks");
+    addUser(user1);
+    User user2 = new User("test2@mail.com", "jdfjkjd");
+    addUser(user2);
+    saveToFile();
 
+//    JSONArray company = new JSONArray();
+//    company.add("Company: Facebook");
+//    obj.put("Company List", company);
   }
 
   public boolean addUser(User user) {
+    JSONObject newUser = new JSONObject();
+    newUser.put("id", users_list.size());
+    newUser.put("email", user.getUserEmail());
+    newUser.put("hash", user.getUserHash());
+    users_list.add(newUser);
     return true;
   }
 
@@ -36,14 +57,22 @@ class DataStore  {
     return true;
   }
 
-
   public void clear() {
 
   }
 
-  private boolean saveToFile(Object src, File dest) {
-
-    return false;
+  private boolean saveToFile() {
+    obj.put("users", users_list);
+    obj.put("links", links_list);
+    try {
+//      File file = new File(fileName);
+      fileWriter = new FileWriter(fileName);
+      fileWriter.write(obj.toJSONString());
+      fileWriter.close();
+      return true;
+    } catch (IOException e) {
+      e.printStackTrace();
+      return false;
+    }
   }
-
 }
